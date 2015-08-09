80f4f689d40cbe57ccb727c31e9eb6bfce9674e5
Dominik Deren <dominik.deren@live.com>
2015-08-09 19:22:35 +0100

    Adding the lambda function handler.

--- lambda/index.js
+++ lambda/index.js
@@ -1,1 +1,60 @@
-// HERE GOES THE CODE FOR LAMBDA HANDELR.
+console.log('Loading function');
+
+require('babel/register');
+var render = require('./server/render');
+
+function JSONize(str) {
+  return str
+    .replace(/([\$\w]+)\s*:/g, function(_, $1){return '"'+$1+'":'})
+    .replace(/:([\$\w]+)\s*/g, function(_, $1){return ':"'+$1+'"'})
+    .replace(/'([^']+)'/g, function(_, $1){return '"'+$1+'"'});
+}
+
+function parse(str) {
+  return JSON.parse(JSONize(str.replace(/=/g, ':')));
+}
+
+function createFullPath(pathParts, queryString) {
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+
+  var path = '/';
+
+  path += Object.keys(pathParts).sort(compareNumbers).map(function (partKey) {
+    return pathParts[partKey];
+  }).join('/');
+
+  var queryStringKeys = Object.keys(queryString);
+
+  if(queryStringKeys.length > 0) {
+    path += '?';
+
+    path += queryStringKeys.map(function (queryStringKey) {
+        return queryStringKey + '=' + queryString[queryStringKey];
+    }).join('&');
+  }
+
+  return path;
+}
+
+exports.handler = function(event, context) {
+  var path, querystring, fullPath, error, renderedContent;
+  try {
+      path = parse(event.path);
+      querystring = parse(event.querystring);
+      fullPath = createFullPath(path, querystring);
+      render.render(fullPath).then(function (arr) {
+        var urlPath = arr[0];
+        var renderedContent = arr[1];
+        context.succeed({path: path, querystring: querystring, fullPath: fullPath, error: error, renderedContent: renderedContent});
+      }).catch(function (error) {
+        console.log('ERROR WHILE RENDERING: ', e);
+      });
+  } catch (e) {
+      error = e;
+      console.log('ERROR: ', e);
+  }
+
+
+};

01bbb4ee3d76c086ab61fc831afdba88bb97a7a8
Dominik Deren <dominik.deren@live.com>
2015-08-16 00:56:29 +0100

    Second attempt at auto-deploy

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div id="mainDiv">Home Page with autodeploy!</div>
+      <div id="mainDiv">Home Page with autodeploy take 2!</div>
     );
   }
 }

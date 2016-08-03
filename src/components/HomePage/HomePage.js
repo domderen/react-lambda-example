85f51c3cc9f253ed43f407330c1dc9b72209e2e8
jamesboyd2008 <jamesboyd2008@gmail.com>
2016-08-02 18:14:56 -0700

    Update file

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class HomePage extends React.Component {
   render() {
     return (
-      <div id="mainDiv">Home Page after major refactoring!</div>
+      <div id="mainDiv">It's working!</div>
     );
   }
 }
